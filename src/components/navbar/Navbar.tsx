651f8d0fa37d5370a5768246b42490f6669560db
Katia Zd <katia.zouad@hotmail.fr>
2024-07-17 18:11:36 +0200

    fix bug: navbar for receipt

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,51 +1,52 @@
-import React, { useState } from "react";
+import React from "react";
 import Link from "next/link";
-import { useSession, signIn, signOut } from "next-auth/react";
+import { useSession, signIn } from "next-auth/react";
 import Image from "next/image";
 import styles from "./Navbar.module.scss";
 
 const Navbar: React.FC = () => {
-  const { data: session, status } = useSession();
+  const { data: session } = useSession();
 
   return (
     <nav className={styles.navbar}>
       <div className={styles.container}>
-        <Link href="/" className={styles.logo}>
-          <Image
-            src="/assets/img/logo.png"
-            alt="Algeriescapades"
-            layout="fill"
-          />
-        </Link>
-        <div className={styles.icons}>
+        <div className={styles.logoContainer}>
+          <Link href="/" className={styles.logo}>
+            <Image
+              src="/assets/img/logo.png"
+              alt="Algeriescapades"
+              width={150}
+              height={80}
+            />
+          </Link>
+        </div>
+        <div className={styles.iconsContainer}>
+          <Link href="/favorites" className={styles.link}>
+            <Image
+              src="/assets/icons/heart.png"
+              alt="Favorites"
+              width={30}
+              height={30}
+              className={styles.icon}
+            />
+          </Link>
           {session ? (
-            <>
-              <Link href="/profile" className={styles.link}>
-                <Image
-                  src="/assets/icons/user.png"
-                  alt="Profile"
-                  layout="fill"
-                  className={styles.icon}
-                />
-              </Link>
-              <button className={styles.iconButton} onClick={() => signOut()}>
-                <Image
-                  src="/assets/icons/heart.png"
-                  alt="Logout"
-                  layout="fill"
-                  className={styles.icon}
-                />
-              </button>
-            </>
-          ) : (
-            <button
-              className={styles.iconButton}
-              onClick={() => signIn("facebook")}
-            >
+            <Link href="/profile" className={styles.link}>
               <Image
                 src="/assets/icons/user.png"
-                alt="Login with Facebook"
-                layout="fill"
+                alt="Profile"
+                width={30}
+                height={30}
+                className={styles.icon}
+              />
+            </Link>
+          ) : (
+            <button className={styles.iconButton} onClick={() => signIn()}>
+              <Image
+                src="/assets/icons/user.png"
+                alt="Login"
+                width={30}
+                height={30}
                 className={styles.icon}
               />
             </button>
