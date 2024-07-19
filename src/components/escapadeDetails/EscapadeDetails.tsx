a9fe1cfd08f7d00053b7af2eb77606d3b647f17e
Katia Zd <katia.zouad@hotmail.fr>
2024-07-19 21:15:37 +0200

    feat: ReservationForm style

--- src/components/escapadeDetails/EscapadeDetails.tsx
+++ src/components/escapadeDetails/EscapadeDetails.tsx
@@ -1,6 +1,74 @@
+// /* eslint-disable react/no-unescaped-entities */
+// "use client";
+// import React from "react";
+// import Image from "next/image";
+// import styles from "../escapadeDetails/EscapadeDetails.module.scss"; 
+// import ReservationForm from "../reservationForm/ReservationForm"; 
+
+// type Escapade = {
+//   id: number;
+//   titre: string;
+//   description: string;
+//   description_principale: string;
+//   votre_escapade: string;
+//   info_pratique: string;
+//   region: string;
+//   duree: number;
+//   prix: number;
+//   photo?: { id: number; url_photo: string }[];
+// };
+
+// const EscapadeDetails: React.FC<{ escapade: Escapade }> = ({ escapade }) => {
+//   return (
+//     <div className={styles.detailsContainer}>
+//       <div className={styles.banner}>
+//         <div className={styles.bannerContent}>
+//           <h1>{escapade.titre}</h1>
+//           <p>{escapade.prix} € par personne</p>
+//           <p>{escapade.description}</p>
+//           <button className={styles.cta}>Réserver</button>
+//         </div>
+//         <Image
+//           src={
+//             escapade.photo
+//               ? escapade.photo[0].url_photo
+//               : "/img/default-photo.jpg"
+//           }
+//           alt={escapade.titre}
+//           layout="fill"
+//           objectFit="cover"
+//         />
+//       </div>
+
+//       <div className={styles.mainContent}>
+//         <div className={styles.textContent}>
+//           <h2>{escapade.titre}</h2>
+//           <p>{escapade.description_principale}</p>
+//           <h3>Votre escapade</h3>
+//           <p>{escapade.votre_escapade}</p>
+//           <h3>Infos pratiques</h3>
+//           <p>{escapade.info_pratique}</p>
+//         </div>
+//         <div className={styles.reservation}>
+//           <h2>Réservation</h2>
+//           <div className={styles.reservationForm}>
+//             <ReservationForm
+//               escapadeId={escapade.id}
+//               escapadePrice={escapade.prix}
+//             />
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default EscapadeDetails;
+
+
 /* eslint-disable react/no-unescaped-entities */
 "use client";
-import React from "react";
+import React, { useRef } from "react";
 import Image from "next/image";
 import styles from "../escapadeDetails/EscapadeDetails.module.scss"; 
 import ReservationForm from "../reservationForm/ReservationForm"; 
@@ -19,6 +87,14 @@ type Escapade = {
 };
 
 const EscapadeDetails: React.FC<{ escapade: Escapade }> = ({ escapade }) => {
+  const reservationSectionRef = useRef<HTMLDivElement>(null);
+
+  const handleReserveClick = () => {
+    if (reservationSectionRef.current) {
+      reservationSectionRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <div className={styles.detailsContainer}>
       <div className={styles.banner}>
@@ -26,10 +102,16 @@ const EscapadeDetails: React.FC<{ escapade: Escapade }> = ({ escapade }) => {
           <h1>{escapade.titre}</h1>
           <p>{escapade.prix} € par personne</p>
           <p>{escapade.description}</p>
-          <button className={styles.cta}>Réserver</button>
+          <button onClick={handleReserveClick} className={styles.cta}>
+            Réserver
+          </button>
         </div>
         <Image
-          src={escapade.photo ? escapade.photo[0].url_photo : "/img/default-photo.jpg"}
+          src={
+            escapade.photo
+              ? escapade.photo[0].url_photo
+              : "/img/default-photo.jpg"
+          }
           alt={escapade.titre}
           layout="fill"
           objectFit="cover"
@@ -45,12 +127,14 @@ const EscapadeDetails: React.FC<{ escapade: Escapade }> = ({ escapade }) => {
           <h3>Infos pratiques</h3>
           <p>{escapade.info_pratique}</p>
         </div>
-        <div className={styles.reservation}>
+        <div className={styles.reservation} ref={reservationSectionRef}>
           <h2>Réservation</h2>
-          <ReservationForm
-            escapadeId={escapade.id}
-            escapadePrice={escapade.prix}
-          />
+          <div className={styles.reservationForm}>
+            <ReservationForm
+              escapadeId={escapade.id}
+              escapadePrice={escapade.prix}
+            />
+          </div>
         </div>
       </div>
     </div>
